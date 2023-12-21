c908e8b51dd30c084806fce76a480c8360b974d6
OsaeAddo <immanueloaddo@gmail.com>
2023-12-21 23:02:58 +0000

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:universal_platform/universal_platform.dart';
+
+void main() {
+  runApp(const TheMovieCatalogue());
+}
+
+
+class TheMovieCatalogue extends StatelessWidget {
+  const TheMovieCatalogue({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "The Movie Catalogue",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Scaffold(),
+    );
+  }
+}
