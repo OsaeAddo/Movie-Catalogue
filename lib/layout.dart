8cb2e9748e2e481d3237ea7a5255e5c7c5560d03
OsaeAddo <immanueloaddo@gmail.com>
2023-12-22 11:45:33 +0000

    add assets

---
+++ lib/layout.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppLayout extends StatefulWidget {
+  const AppLayout({super.key});
+
+  @override
+  State<AppLayout> createState() => _AppLayoutState();
+}
+
+class _AppLayoutState extends State<AppLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
