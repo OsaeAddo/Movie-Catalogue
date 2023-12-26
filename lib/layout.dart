3ab0a021a2472f6d10aa3d89b091502199e9f1c7
OsaeAddo <immanueloaddo@gmail.com>
2023-12-26 07:55:26 +0000

    update app UI layout

--- lib/layout.dart
+++ lib/layout.dart
@@ -12,6 +12,48 @@ class _AppLayoutState extends State<AppLayout> {
   @override
   Widget build(BuildContext context) {
     //Start building the Interface
-    return Container();
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage("assets/img/bg.jpg"), fit: BoxFit.cover),
+      ),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Left Pane Section
+            Container(
+              width: 300,
+              child: Column(),
+              color: Colors.indigo.withOpacity(0.95),
+            ),
+            //2nd Column for Headers & Main Pane
+            Expanded(
+                child: Column(
+              children: [
+                //Main Header Section
+                Container(
+                  height: 120,
+                  color: Colors.indigo.withOpacity(0.8),
+                  child: Row(),
+                ),
+                //Filter Section
+                Container(
+                  height: 120,
+                  color: Colors.deepPurple.withOpacity(0.6),
+                  child: Row(),
+                ),
+                //Main Pane Section
+                const Expanded(
+                    child: Center(
+                  child: Text("Helloooooo World"),
+                ))
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
   }
 }
